9bae2321923479175e8a4922687ec8277cd72b45
Phitzi <phitzi@gmail.com>
2024-03-31 11:21:56 +0200

    Create sendemail.py

---
+++ sendemail.py
@@ -0,0 +1,30 @@
+def send_email(subject, html_body):
+    try:
+        smtp_user = os.environ["SMTP_USER"]
+        smtp_password = os.environ["SMTP_PASSWORD"]
+        smtp_server = os.environ["SMTP_SERVER"]
+        smtp_port = int(os.environ["SMTP_PORT"])
+        sender_email = os.environ["SENDER_EMAIL"]
+        to_email = os.environ["TO_EMAIL"]
+
+        # Construct the email message
+        message = MIMEMultipart()
+        message['From'] = sender_email
+        message['To'] = to_email
+        message['Subject'] = subject
+        message.attach(MIMEText(html_body, 'html'))
+
+        # Connect to the SMTP server
+        with smtplib.SMTP(smtp_server, smtp_port) as server:
+            # Start TLS for security (optional)
+            server.starttls()
+
+            # Log in to the email account
+            server.login(smtp_user, smtp_password)
+
+            # Send the email
+            server.sendmail(sender_email, to_email, message.as_string())
+
+        print('Email sent successfully!')
+    except Exception as e:
+        print(e)
